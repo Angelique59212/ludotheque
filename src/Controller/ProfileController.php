eccfbba1ee3535dc38add6a845545345c179a843
angelika <dehainaut.angelique@orange.fr>
2023-10-24 10:35:03 +0200

    create profile

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+/*
+ *reserved for authenticated members
+ */
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    public function index(): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+}
