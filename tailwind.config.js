7707ee69f06e8992e752ccbf76574458e0a70058
angelika <dehainaut.angelique@orange.fr>
2023-10-24 22:29:30 +0200

    register and carousel home

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
         'h1Home': '#1C5D99',
         'pColor' : '#222222',
         'footer' : '#222222',
+        'button': '#A6D7D7',
       },
       fontFamily: {
         'popp': ['Poppins', 'sans-serif'],
@@ -20,6 +21,9 @@ module.exports = {
         'h1': '36px',
         'textp': '24px',
       }
+    },
+    backgroundImage: {
+      'form-gradient': 'linear-gradient(180deg, #6AADBA 0%, rgba(220, 239, 242, 0.00) 60.42%, rgba(202, 235, 242, 0.28) 77.6%)',
     },
     // screens: {
     //
